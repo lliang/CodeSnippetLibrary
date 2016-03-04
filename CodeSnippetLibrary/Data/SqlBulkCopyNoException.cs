487d61f3b75310074862f26e0bd90234adef9f08
liuliang <lliang@outlook.com>
2016-03-04 10:48:41 +0800

    Add data processing related classes

---
+++ CodeSnippetLibrary/Data/SqlBulkCopyNoException.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Data;
+using System.Threading;
+using System.Data.SqlClient;
+
+namespace CodeSnippetLibrary.Data
+{
+    public class SqlBulkCopyNoException : IDisposable
+    {
+        private const int BatchSize = 1000; //批处理数据条数
+        private const int BulkCopyTimeout = 3600;
+
+        readonly SqlConnection _conn;
+        readonly SqlBulkCopy _bulkCopy;
+        private DataTable _importdata;
+
+        public SqlBulkCopyNoException(string connString, string tableName)
+        {
+            _conn = new SqlConnection(connString);
+            _bulkCopy = new SqlBulkCopy(_conn)
+            {
+                DestinationTableName = tableName,
+                BatchSize = BatchSize,
+                BulkCopyTimeout = BulkCopyTimeout
+            };
+        }
+
+        public void Import(DataTable dataTable)
+        {
+            if (dataTable == null || dataTable.Rows.Count == 0)
+            {
+                return;
+            }
+
+            try
+            {
+                _importdata = dataTable;
+                Open();
+                _bulkCopy.WriteToServer(_importdata);
+            }
+            catch (Exception)
+            {
+                if (dataTable.Rows.Count == 1)
+                {
+                    return;
+                }
+                int middle = dataTable.Rows.Count / 2;
+                DataTable table = dataTable.Clone();
+                for (int i = 0; i < middle; i++)
+                {
+                    table.ImportRow(dataTable.Rows[i]);
+                }
+                Import(table);
+
+                table.Clear();
+                for (int i = middle; i < dataTable.Rows.Count; i++)
+                {
+                    table.ImportRow(dataTable.Rows[i]);
+                }
+                Import(table);
+            }
+        }
+
+        public void Dispose()
+        {
+            Close();
+        }
+
+        private void Close()
+        {
+            _importdata = null;
+            if (_conn != null && _conn.State != ConnectionState.Closed)
+            {
+                _conn.Close();
+            }
+            if (_bulkCopy != null)
+            {
+                _bulkCopy.Close();
+            }
+        }
+
+        private void Open()
+        {
+            if (_conn.State != ConnectionState.Closed) return;
+            try
+            {
+                _conn.Open();
+            }
+            catch (SqlException)
+            {
+                Thread.Sleep(600000);
+                Open();
+            }
+            catch (Exception)
+            {
+                Thread.Sleep(600000);
+                Open();
+            }
+        }
+    }
+}
---
+++ CodeSnippetLibrary/Data/SqlExecutor.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CodeSnippetLibrary.Data
+{
+    public class SqlExecutor : IDisposable
+    {
+        public int ExecuteNonQuery(SqlCommand cmd, string connectionString)
+        {
+            return (int)CommandExecute(cmd, connectionString, CommandExecuteType.NonQuery);
+        }
+
+        public SqlDataReader ExecuteReader(SqlCommand cmd, string connectionString)
+        {
+            return (SqlDataReader)CommandExecute(cmd, connectionString, CommandExecuteType.DataReader);
+        }
+
+        public TValue ExecuteScalar<TValue>(SqlCommand cmd, string connectionString)
+        {
+            object result = CommandExecute(cmd, connectionString, CommandExecuteType.Scalar);
+            if (result == DBNull.Value)
+            {
+                result = null;
+            }
+
+            //先尝试强制类型转换，如果遇到值类型失败了，则尝试用Convert转换，如果再失败则正常抛出异常
+            try
+            {
+                return (TValue)result;
+            }
+            catch
+            {
+                return (TValue)Convert.ChangeType(result, typeof(TValue));
+            }
+        }
+
+        #region Close & Dispose
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        private void Dispose(bool disposing)
+        {
+            if (!_isDisposed)
+            {
+                if (disposing)
+                {
+                    Close();
+                }
+                _isDisposed = true;
+            }
+        }
+
+        public void Close()
+        {
+            if (_currentSqlDataReader != null)
+            {
+                _currentSqlDataReader.Close();
+                _currentSqlDataReader.Dispose();
+                _currentSqlDataReader = null;
+            }
+
+            if (_currentSqlCommand != null)
+            {
+                _currentSqlCommand.Dispose();
+                _currentSqlCommand = null;
+            }
+
+            if (_currentSqlConnection != null)
+            {
+                _currentSqlConnection.Close();
+                _currentSqlConnection.Dispose();
+                _currentSqlConnection = null;
+            }
+        }
+
+        #endregion
+
+        #region Private
+
+        private object CommandExecute(SqlCommand cmd, string cs, CommandExecuteType type)
+        {
+            object result;
+            cmd.CommandTimeout = 3600;
+            _currentSqlCommand = cmd;
+            _currentSqlConnection = SetSqlConnection(cmd, cs);
+
+            _currentSqlConnection.Open();
+
+            switch (type)
+            {
+                case CommandExecuteType.DataReader:
+                    _currentSqlDataReader = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+                    result = _currentSqlDataReader;
+                    break;
+                case CommandExecuteType.NonQuery:
+                    result = cmd.ExecuteNonQuery();
+                    break;
+                case CommandExecuteType.Scalar:
+                    result = cmd.ExecuteScalar();
+                    break;
+                default:
+                    result = null;
+                    break;
+            }
+
+            if (type != CommandExecuteType.DataReader)
+            {
+                _currentSqlCommand.Dispose();
+                _currentSqlCommand = null;
+
+                _currentSqlConnection.Close();
+                _currentSqlConnection.Dispose();
+                _currentSqlConnection = null;
+            }
+
+            return result;
+        }
+
+        private static SqlConnection SetSqlConnection(SqlCommand cmd, string cs)
+        {
+            SqlConnection connection = new SqlConnection(cs);
+            cmd.Connection = connection;
+            return connection;
+        }
+
+        private bool _isDisposed;
+        private SqlCommand _currentSqlCommand;
+        private SqlDataReader _currentSqlDataReader;
+        private SqlConnection _currentSqlConnection;
+
+        private enum CommandExecuteType
+        {
+            NonQuery = 1,
+            Scalar = 2,
+            DataReader = 3
+        }
+
+        #endregion
+    }
+}
